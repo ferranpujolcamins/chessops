38ba39a280f6bb41cb72c7669c61a5df2e481fa9
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-04-21 15:36:38 +0200

    try arrow methods for SquareSet

--- src/squareSet.ts
+++ src/squareSet.ts
@@ -1,22 +1,22 @@
 import { Square, Color } from './types.js';
 
-function popcnt32(n: number): number {
+const popcnt32 = (n: number) => {
   n = n - ((n >>> 1) & 0x5555_5555);
   n = (n & 0x3333_3333) + ((n >>> 2) & 0x3333_3333);
   return Math.imul((n + (n >>> 4)) & 0x0f0f_0f0f, 0x0101_0101) >> 24;
-}
+};
 
-function bswap32(n: number): number {
+const bswap32 = (n: number) => {
   n = ((n >>> 8) & 0x00ff_00ff) | ((n & 0x00ff_00ff) << 8);
   return ((n >>> 16) & 0xffff) | ((n & 0xffff) << 16);
-}
+};
 
-function rbit32(n: number): number {
+const rbit32 = (n: number) => {
   n = ((n >>> 1) & 0x5555_5555) | ((n & 0x5555_5555) << 1);
   n = ((n >>> 2) & 0x3333_3333) | ((n & 0x3333_3333) << 2);
   n = ((n >>> 4) & 0x0f0f_0f0f) | ((n & 0x0f0f_0f0f) << 4);
   return bswap32(n);
-}
+};
 
 export class SquareSet implements Iterable<Square> {
   constructor(readonly lo: number, readonly hi: number) {
@@ -68,136 +68,94 @@ export class SquareSet implements Iterable<Square> {
     return new SquareSet(0xaa55_aa55, 0xaa55_aa55);
   }
 
-  complement(): SquareSet {
-    return new SquareSet(~this.lo, ~this.hi);
-  }
+  complement = () => new SquareSet(~this.lo, ~this.hi);
 
-  xor(other: SquareSet): SquareSet {
-    return new SquareSet(this.lo ^ other.lo, this.hi ^ other.hi);
-  }
+  xor = (other: SquareSet) => new SquareSet(this.lo ^ other.lo, this.hi ^ other.hi);
 
-  union(other: SquareSet): SquareSet {
-    return new SquareSet(this.lo | other.lo, this.hi | other.hi);
-  }
+  union = (other: SquareSet) => new SquareSet(this.lo | other.lo, this.hi | other.hi);
 
-  intersect(other: SquareSet): SquareSet {
-    return new SquareSet(this.lo & other.lo, this.hi & other.hi);
-  }
+  intersect = (other: SquareSet) => new SquareSet(this.lo & other.lo, this.hi & other.hi);
 
-  diff(other: SquareSet): SquareSet {
-    return new SquareSet(this.lo & ~other.lo, this.hi & ~other.hi);
-  }
+  diff = (other: SquareSet) => new SquareSet(this.lo & ~other.lo, this.hi & ~other.hi);
 
-  intersects(other: SquareSet): boolean {
-    return this.intersect(other).nonEmpty();
-  }
+  intersects = (other: SquareSet) => this.intersect(other).nonEmpty();
 
-  isDisjoint(other: SquareSet): boolean {
-    return this.intersect(other).isEmpty();
-  }
+  isDisjoint = (other: SquareSet) => this.intersect(other).isEmpty();
 
-  supersetOf(other: SquareSet): boolean {
-    return other.diff(this).isEmpty();
-  }
+  supersetOf = (other: SquareSet) => other.diff(this).isEmpty();
 
-  subsetOf(other: SquareSet): boolean {
-    return this.diff(other).isEmpty();
-  }
+  subsetOf = (other: SquareSet) => this.diff(other).isEmpty();
 
-  shr64(shift: number): SquareSet {
+  shr64 = (shift: number) => {
     if (shift >= 64) return SquareSet.empty();
     if (shift >= 32) return new SquareSet(this.hi >>> (shift - 32), 0);
     if (shift > 0) return new SquareSet((this.lo >>> shift) ^ (this.hi << (32 - shift)), this.hi >>> shift);
     return this;
-  }
+  };
 
-  shl64(shift: number): SquareSet {
+  shl64 = (shift: number) => {
     if (shift >= 64) return SquareSet.empty();
     if (shift >= 32) return new SquareSet(0, this.lo << (shift - 32));
     if (shift > 0) return new SquareSet(this.lo << shift, (this.hi << shift) ^ (this.lo >>> (32 - shift)));
     return this;
-  }
+  };
 
-  bswap64(): SquareSet {
-    return new SquareSet(bswap32(this.hi), bswap32(this.lo));
-  }
+  bswap64 = () => new SquareSet(bswap32(this.hi), bswap32(this.lo));
 
-  rbit64(): SquareSet {
-    return new SquareSet(rbit32(this.hi), rbit32(this.lo));
-  }
+  rbit64 = () => new SquareSet(rbit32(this.hi), rbit32(this.lo));
 
-  minus64(other: SquareSet): SquareSet {
+  minus64 = (other: SquareSet) => {
     const lo = this.lo - other.lo;
     const c = ((lo & other.lo & 1) + (other.lo >>> 1) + (lo >>> 1)) >>> 31;
     return new SquareSet(lo, this.hi - (other.hi + c));
-  }
+  };
 
-  equals(other: SquareSet): boolean {
-    return this.lo === other.lo && this.hi === other.hi;
-  }
+  equals = (other: SquareSet) => this.lo === other.lo && this.hi === other.hi;
 
-  size(): number {
-    return popcnt32(this.lo) + popcnt32(this.hi);
-  }
+  size = () => popcnt32(this.lo) + popcnt32(this.hi);
 
-  isEmpty(): boolean {
-    return this.lo === 0 && this.hi === 0;
-  }
+  isEmpty = () => this.lo === 0 && this.hi === 0;
 
-  nonEmpty(): boolean {
-    return this.lo !== 0 || this.hi !== 0;
-  }
+  nonEmpty = () => this.lo !== 0 || this.hi !== 0;
 
-  has(square: Square): boolean {
-    return (square >= 32 ? this.hi & (1 << (square - 32)) : this.lo & (1 << square)) !== 0;
-  }
+  has = (square: Square) => (square >= 32 ? this.hi & (1 << (square - 32)) : this.lo & (1 << square)) !== 0;
 
-  set(square: Square, on: boolean): SquareSet {
-    return on ? this.with(square) : this.without(square);
-  }
+  set = (square: Square, on: boolean) => (on ? this.with(square) : this.without(square));
 
-  with(square: Square): SquareSet {
-    return square >= 32
+  with = (square: Square) =>
+    square >= 32
       ? new SquareSet(this.lo, this.hi | (1 << (square - 32)))
       : new SquareSet(this.lo | (1 << square), this.hi);
-  }
 
-  without(square: Square): SquareSet {
-    return square >= 32
+  without = (square: Square) =>
+    square >= 32
       ? new SquareSet(this.lo, this.hi & ~(1 << (square - 32)))
       : new SquareSet(this.lo & ~(1 << square), this.hi);
-  }
 
-  toggle(square: Square): SquareSet {
-    return square >= 32
+  toggle = (square: Square) =>
+    square >= 32
       ? new SquareSet(this.lo, this.hi ^ (1 << (square - 32)))
       : new SquareSet(this.lo ^ (1 << square), this.hi);
-  }
 
-  last(): Square | undefined {
+  last = (): Square | undefined => {
     if (this.hi !== 0) return 63 - Math.clz32(this.hi);
     if (this.lo !== 0) return 31 - Math.clz32(this.lo);
     return;
-  }
+  };
 
-  first(): Square | undefined {
+  first = (): Square | undefined => {
     if (this.lo !== 0) return 31 - Math.clz32(this.lo & -this.lo);
     if (this.hi !== 0) return 63 - Math.clz32(this.hi & -this.hi);
     return;
-  }
+  };
 
-  withoutFirst(): SquareSet {
-    if (this.lo !== 0) return new SquareSet(this.lo & (this.lo - 1), this.hi);
-    return new SquareSet(0, this.hi & (this.hi - 1));
-  }
+  withoutFirst = () =>
+    this.lo !== 0 ? new SquareSet(this.lo & (this.lo - 1), this.hi) : new SquareSet(0, this.hi & (this.hi - 1));
 
-  moreThanOne(): boolean {
-    return (this.hi !== 0 && this.lo !== 0) || (this.lo & (this.lo - 1)) !== 0 || (this.hi & (this.hi - 1)) !== 0;
-  }
+  moreThanOne = () =>
+    (this.hi !== 0 && this.lo !== 0) || (this.lo & (this.lo - 1)) !== 0 || (this.hi & (this.hi - 1)) !== 0;
 
-  singleSquare(): Square | undefined {
-    return this.moreThanOne() ? undefined : this.last();
-  }
+  singleSquare = (): Square | undefined => (this.moreThanOne() ? undefined : this.last());
 
   *[Symbol.iterator](): Iterator<Square> {
     let lo = this.lo;
